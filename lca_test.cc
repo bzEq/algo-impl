40b6a0f9f07a0b6e4726bdc207694a72c9f11220
Kai Luo <gluokai@gmail.com>
2020-04-18 20:11:58 +0800

    Add a non-trivial impl

--- lca_test.cc
+++ lca_test.cc
@@ -6,17 +6,21 @@ struct LCA {
   const Graph &graph;
   const size_t size;
   const unsigned UNDEF;
-  std::vector<unsigned> tree_parent, label;
+  std::vector<unsigned> tree_parent, label, height;
   struct Range {
     unsigned begin, end;
   };
   std::vector<Range> dfo;
+  std::vector<std::vector<unsigned>> ancestor;
 
   LCA(const Graph &graph)
       : graph(graph), size(graph.succ.size()), UNDEF(size),
-        tree_parent(size, UNDEF), label(size, UNDEF), dfo(size) {
-    unsigned depth_first_order = 0;
+        tree_parent(size, UNDEF), label(size, UNDEF), height(size, UNDEF),
+        dfo(size), ancestor(size) {
+    unsigned depth_first_order = 0, current_height = 0;
     auto pre_visit = [&](unsigned parent, unsigned u) {
+      height[u] = current_height;
+      ++current_height;
       dfo[u].begin = depth_first_order;
       dfo[u].end = depth_first_order;
       label[depth_first_order] = u;
@@ -28,25 +32,80 @@ struct LCA {
       if (parent != UNDEF) {
         dfo[parent].end = std::max(dfo[parent].end, dfo[u].end);
       }
+      assert(current_height > 0);
+      --current_height;
     };
     IterativeDepthDirstVisit(graph, pre_visit, non_tree_visit, post_visit);
+    const unsigned MAX_ORDER = Log2Ceil(size);
+    for (unsigned u = 0; u < size; ++u) {
+      if (height[u] != 0 && height[u] != UNDEF)
+        ancestor[u].resize(Log2Ceil(height[u]) + 1, label[0]);
+    }
+    for (unsigned o = 0; o < MAX_ORDER; ++o) {
+      for (unsigned dfn = 0; dfn < size; ++dfn) {
+        unsigned u = label[dfn];
+        if (o == 0) {
+          if (tree_parent[u] != UNDEF && 0 < ancestor[u].size()) {
+            ancestor[u][0] = tree_parent[u];
+          }
+        } else {
+          if (o < ancestor[u].size()) {
+            unsigned v = ancestor[u][o - 1];
+            if (o - 1 < ancestor[v].size())
+              ancestor[u][o] = ancestor[v][o - 1];
+          }
+        }
+      }
+    }
+  }
+
+  bool Covers(unsigned u, unsigned v) {
+    return dfo[u].begin <= dfo[v].begin && dfo[v].begin <= dfo[u].end;
+  }
+
+  unsigned GetAncestor(unsigned u, unsigned distance) {
+    if (distance == 0)
+      return u;
+    unsigned o = Log2Ceil(distance);
+    if (o >= ancestor[u].size())
+      return label[0];
+    return GetAncestor(ancestor[u][o], distance - (1 << o));
   }
 
   unsigned GetLCA(unsigned u, unsigned v) {
     assert(u < size && v < size);
-    if (dfo[u].begin > dfo[v].begin)
+    if (height[u] < height[v]) {
       std::swap(u, v);
-    if (dfo[u].begin <= dfo[v].begin && dfo[v].begin <= dfo[u].end)
+    }
+    unsigned distance = height[u] - height[v];
+    u = GetAncestor(u, distance);
+    assert(height[u] == height[v]);
+    assert(ancestor[u].size() == ancestor[v].size());
+    if (u == v)
       return u;
-    assert(dfo[v].begin > dfo[u].end);
-    assert(dfo[u].end + 1 < size);
-    unsigned w = label[dfo[u].end + 1];
-    assert(tree_parent[w] != UNDEF);
-    if (v == w)
-      return tree_parent[w];
-    return GetLCA(tree_parent[w], v);
+    unsigned o = ancestor[u].size();
+    for (int i = o - 1; i >= 0; --i) {
+      if (height[ancestor[u][i]] >= height[v])
+        u = ancestor[u][i];
+    }
+    o = ancestor[u].size();
+    for (int i = o - 1; i >= 0; --i) {
+      if (ancestor[u][i] != ancestor[v][i]) {
+        u = ancestor[u][i];
+        v = ancestor[v][i];
+      }
+    }
+    return ancestor[u][0];
   }
 };
+
+TEST(LCATest, Log2Ceil) {
+  EXPECT_TRUE(Log2Ceil(2) == 1);
+  EXPECT_TRUE(Log2Ceil(4) == 2);
+  EXPECT_TRUE(Log2Ceil(6) == 3);
+  EXPECT_TRUE(Log2Ceil(12) == 4);
+  EXPECT_TRUE(Log2Ceil(16) == 4);
+}
 
 TEST(LCATest, Simple) {
   Graph g(8);
@@ -58,14 +117,25 @@ TEST(LCATest, Simple) {
   g.AddEdge(5, 6);
   g.AddEdge(5, 7);
   LCA lca(g);
+  EXPECT_TRUE(lca.height[4] == 2);
+  EXPECT_TRUE(lca.ancestor[4].size() == 2);
+  EXPECT_TRUE(lca.ancestor[4][0] == 1);
+  EXPECT_TRUE(lca.ancestor[4][1] == 0);
   EXPECT_TRUE(lca.tree_parent[4] == 1);
   EXPECT_TRUE(lca.GetLCA(0, 0) == 0);
   EXPECT_TRUE(lca.GetLCA(1, 2) == 1);
   EXPECT_TRUE(lca.GetLCA(3, 4) == 1);
   EXPECT_TRUE(lca.label[lca.dfo[1].end] == 4);
   EXPECT_TRUE(lca.GetLCA(1, 5) == 0);
+  EXPECT_TRUE(lca.height[3] == 3);
+  EXPECT_TRUE(lca.ancestor[3].size() == 3);
+  EXPECT_TRUE(lca.ancestor[3][0] == 2);
+  EXPECT_TRUE(lca.ancestor[3][1] == 1);
+  EXPECT_TRUE(lca.ancestor[3][2] == 0);
   EXPECT_TRUE(lca.GetLCA(3, 5) == 0);
   EXPECT_TRUE(lca.GetLCA(4, 5) == 0);
   EXPECT_TRUE(lca.GetLCA(6, 7) == 5);
+  EXPECT_TRUE(lca.ancestor[3].back() == 0);
+  EXPECT_TRUE(lca.ancestor[6].back() == 0);
   EXPECT_TRUE(lca.GetLCA(3, 6) == 0);
 }
