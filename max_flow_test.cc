2811a296a446c833b9e4e2ebbc47a823f43d141c
Kai Luo <gluokai@gmail.com>
2020-04-21 18:06:12 +0800

    Get framework works

--- max_flow_test.cc
+++ max_flow_test.cc
@@ -1,11 +1,12 @@
 #include "core.h"
 
 #include <gtest/gtest.h>
+#include <limits>
 
 struct FlowGraph {
   std::unique_ptr<Graph> graph;
   const unsigned source, target;
-  std::map<std::tuple<unsigned, unsigned>, int> capacity, residual_capacity;
+  std::map<std::tuple<unsigned, unsigned>, int> residual_capacity;
   std::map<std::tuple<unsigned, unsigned>, int> flow;
 
   explicit FlowGraph(std::unique_ptr<Graph> &graph)
@@ -13,19 +14,11 @@ struct FlowGraph {
 
   bool SetCapacity(const unsigned u, const unsigned v, const int c) {
     assert(u != v);
-    assert(graph->succ[u].count(v));
     assert(c >= 0);
-    return std::get<1>(capacity.insert({{u, v}, c}));
+    return std::get<1>(residual_capacity.insert({{u, v}, c}));
   }
 
   int GetCapacity(const unsigned u, const unsigned v) {
-    auto it = capacity.find({u, v});
-    if (it == capacity.end())
-      return 0;
-    return it->second;
-  }
-
-  int GetResidualCapacity(const unsigned u, const unsigned v) {
     auto it = residual_capacity.find({u, v});
     if (it == residual_capacity.end())
       return 0;
@@ -59,11 +52,37 @@ struct FlowGraph {
 };
 
 struct PushAndRelabel {
+  static const unsigned INF = std::numeric_limits<unsigned>::max();
   FlowGraph &network;
   std::vector<int> excess;
+  std::vector<unsigned> distance;
 
   PushAndRelabel(FlowGraph &network)
-      : network(network), excess(network.graph->succ.size(), 0) {}
+      : network(network), excess(network.graph->succ.size(), 0),
+        distance(network.graph->succ.size(), INF) {
+    distance[network.target] = 0;
+    distance[network.source] = network.graph->succ.size();
+  }
+
+  void Push(const unsigned u, const unsigned v) {
+    int diff = std::min(network.GetCapacity(u, v), excess[u]);
+    network.UpdateFlow(u, v, network.GetFlow(u, v) + diff);
+    excess[u] -= diff;
+    excess[v] += diff;
+  }
+
+  void Relabel(unsigned u) {
+    for (auto v : network.graph->succ[u]) {
+      if (distance[v] != INF)
+        distance[u] = std::min(distance[u], distance[v] + 1);
+    }
+    for (auto v : network.graph->pred[u]) {
+      if (distance[v] != INF)
+        distance[u] = std::min(distance[u], distance[v] + 1);
+    }
+  }
+
+  int CalculateMaxFlow() {}
 };
 
 namespace {
@@ -85,7 +104,7 @@ GenerateRandomFlowGraph(const size_t num_of_vertexes, const size_t num_of_edges,
   return fg;
 }
 
-TEST(MaxFlowTest, FlowGraph) {
+TEST(MaxFlowTest, SimpleFlowGraph) {
   auto g = std::make_unique<Graph>(5, true);
   g->AddEdge(0, 1);
   g->AddEdge(1, 2);
@@ -96,8 +115,8 @@ TEST(MaxFlowTest, FlowGraph) {
   FlowGraph fg(g);
   EXPECT_TRUE(fg.SetCapacity(0, 1, 5));
   EXPECT_FALSE(fg.SetCapacity(0, 1, 6));
-  auto it = fg.capacity.find({0, 1});
-  EXPECT_TRUE(it != fg.capacity.end());
+  auto it = fg.residual_capacity.find({0, 1});
+  EXPECT_TRUE(it != fg.residual_capacity.end());
   EXPECT_TRUE(it->second == 5);
   EXPECT_TRUE(fg.SetCapacity(1, 2, 3));
   EXPECT_TRUE(fg.SetCapacity(1, 3, 5));
